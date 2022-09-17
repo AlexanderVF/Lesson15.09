982f018f0d25bb78ed797c40b3a69a4c7f4f9b06
Alexander.Fedotov <favorite@bk.ru>
2022-09-17 10:46:21 +0300

    hw

---
+++ HW_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+Console.WriteLine("Vvedite M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Vvedite N");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("M -> " + m + "; N -> " + n + ". -> ");
+for (int i = m; i <= n; i++) {
+    Console.Write(i + ", ");
+}
---
+++ HW_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+Console.WriteLine("Vvedite M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Vvedite N");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+Console.Write("M = " + m + "; N = " + n + ". -> ");
+for (int i = m; i <= n; i++) {
+    sum += i;
+    }
+
+Console.WriteLine(sum);
